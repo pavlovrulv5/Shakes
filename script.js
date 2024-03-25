7bbb8771c64af14fd3dbfd9ab4c36b09ab42fe30
pavlovrulv5 <9258857117@mail.ru>
2024-03-25 13:47:54 +0300

    add

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
